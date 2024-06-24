ba1f25ecf11c9fb2cd78429afe456b1b0f06063e
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2024-06-24 19:54:45 +0700

    B1

---
+++ react-ts/src/App.tsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [todos,setTodo]=useState([
+    {id:1,title:"To do 1",complete: true},
+    {id:2,title:"To do 2",complete: true},
+    {id:3,title:"To do 3",complete: true}
+  ])
+  const [count, setCount] = useState(0)
+  const [newtodo,setNewtodo] = useState('')
+  const handleClickAdd =()=>{
+    setTodo([...todos,{id:todos.length+1,title:newtodo,complete:true}])
+  }
+  const Set_new_todo =(value:any)=>{
+    setNewtodo(value)
+  }
+  const onDelete = (id:any)=>{
+    if (confirm("Are you sure you want to delete")){
+      const newtodos = todos.filter(todo=>todo.id!==id)
+      setTodo(newtodos)
+    }
+  }
+  return (
+    <>
+    <input type='text' onChange={(e)=>Set_new_todo(e.target.value)}/>
+    <button onClick={handleClickAdd}>Thêm danh sách</button>
+    <ul>
+      {
+        todos.map(todo=>(
+          <li key={todo.id}> {todo.title} <button onClick={()=>onDelete(todo.id)}>Xóa</button></li>
+        ))
+      }
+      </ul>
+      
+    </>
+  )
+}
+
+export default App
