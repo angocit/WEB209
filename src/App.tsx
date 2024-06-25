bfa8346fdb97e7a130e5ef622e5c9180d1b064d2
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2024-06-25 19:50:04 +0700

    B1

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,24 @@ function App() {
     {id:2,title:"To do 2",complete:true},
     {id:3,title:"To do 3",complete:true}
   ])
+  const [newtodo,setNewtodo] = useState('')
+  const DeleteTodo =(id:number)=>{
+    if(confirm("Are you sure you want to delete")){
+      const newtodolist = todolist.filter(todo=>todo.id !== id)
+      setTodolist(newtodolist) 
+    }
+  }
+  const handleAdd=()=>{
+    setTodolist([...todolist,{id:todolist.length+1,title:newtodo,complete:true}])
+  }
   return (
     <>
      <h1>Danh sách các việc đã làm</h1>
+     <input type='text' placeholder='Nhập gì đó vào đây' onChange={(e)=>{setNewtodo(e.target.value)}}/>
+     <button onClick={handleAdd}>Thêm</button>
      <ul>
      {todolist.map(todo=>(
-        <li>{todo.title}</li>
+        <li>{todo.title} <button onClick={()=>DeleteTodo(todo.id)}>Xóa</button></li>
      ))}
      </ul>
     </>
