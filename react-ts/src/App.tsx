465a994f650e836e369c5809b52c682c854829bb
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2024-06-26 19:02:40 +0700

    B2

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -23,6 +23,25 @@ function App() {
       setTodo(newtodos)
     }
   }
+  const changeStatus = (id:number)=>{
+    const newtodos = todos.map(todo=>{
+      if (todo.id ==id){
+        todo.complete = !todo.complete
+      }
+      return todo
+    })
+    setTodo(newtodos)
+  }
+  const updateTodo = (id:number)=>{
+    const newtodos = todos.map(todo=>{
+      if (todo.id ==id){
+        todo.title = newtodo
+        todo.complete = true
+      }
+      return todo
+    })
+    setTodo(newtodos)
+  }
   return (
     <>
     <input type='text' onChange={(e)=>Set_new_todo(e.target.value)}/>
@@ -30,7 +49,13 @@ function App() {
     <ul>
       {
         todos.map(todo=>(
-          <li key={todo.id}> {todo.title} <button onClick={()=>onDelete(todo.id)}>Xóa</button></li>
+          (todo.complete)?
+          (<li key={todo.id}> {todo.title}<button onClick={()=>{changeStatus(todo.id)}}>Sửa</button> <button onClick={()=>onDelete(todo.id)}>Xóa</button></li>)
+          :(<li key={todo.id}>
+            <input type='text' defaultValue={todo.title} onChange={(e)=>Set_new_todo(e.target.value)}/>
+            <button onClick={()=>updateTodo(todo.id)}>Lưu</button>
+            <button onClick={()=>changeStatus(todo.id)}>Hủy</button>
+          </li>)
         ))
       }
       </ul>
