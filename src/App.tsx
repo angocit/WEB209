30a92f5cbb44eac8a4fb1746a4ffffe36ec7a7ca
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2024-06-27 19:29:53 +0700

    B2

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+interface ITodo {
+  id: number;
+  title: string;
+  complete: boolean;
+}
 function App() {
   const [count, setCount] = useState(0)
-  const [todolist,setTodolist] = useState([
-    {id:1,title:"To do 1",complete:true},
-    {id:2,title:"To do 2",complete:true},
-    {id:3,title:"To do 3",complete:true}
-  ])
+  const [flag,setFlag] = useState(0)
+  const [todolist,setTodolist] = useState<ITodo[]>([])
+  useEffect(()=>{
+    fetch("http://localhost:3000/todos").then(res=>res.json())
+    .then((data:ITodo[])=>{
+      setTodolist(data)
+    })
+  },[])
   const [newtodo,setNewtodo] = useState('')
   const DeleteTodo =(id:number)=>{
     if(confirm("Are you sure you want to delete")){
@@ -21,13 +28,14 @@ function App() {
     setTodolist([...todolist,{id:todolist.length+1,title:newtodo,complete:true}])
   }
   const onChangetodo = (id:number)=>{
-    const newtodos = todolist.map(todo=>{
-      if (todo.id==id){
-        todo.complete = !todo.complete;
-      }
-      return todo
-    })
-    setTodolist(newtodos)
+    setFlag(id)
+    // const newtodos = todolist.map(todo=>{
+    //   if (todo.id==id){
+    //     todo.complete = !todo.complete;
+    //   }
+    //   return todo
+    // })
+    // setTodolist(newtodos)
   }
   const updateTodo = (id:number)=>{
     const newtodos = todolist.map(todo=>{
@@ -46,7 +54,7 @@ function App() {
      <button onClick={handleAdd}>Thêm</button>
      <ul>
      {todolist.map(todo=>(
-        (todo.complete)?
+        (todo.id!==flag)?
         <li>{todo.title} <button onClick={()=>{onChangetodo(todo.id)}}>Sửa</button><button onClick={()=>DeleteTodo(todo.id)}>Xóa</button></li>
       :<li><input type='text' defaultValue={todo.title} onChange={(e)=>{setNewtodo(e.target.value)}}/> <button onClick={()=>updateTodo(todo.id)}>Lưu</button><button onClick={()=>{onChangetodo(todo.id)}}>Hủy</button></li>
       ))}
