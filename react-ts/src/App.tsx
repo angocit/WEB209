0bcc2d07a4fbec053ec03d867ec695719eda410e
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2024-06-28 20:03:33 +0700

    B3

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { useForm, SubmitHandler } from "react-hook-form"
 import axios from 'axios'
 import './App.css'
 interface IProduct {
@@ -10,17 +11,41 @@ interface IProduct {
   price: number;
   category: string;
 }
+type formType = {
+  name: string;
+  image: string;
+  price: number;
+  category: string;
+}
 function App() {
   const [products,setProducts] = useState<IProduct[]>([])
+  const {register,handleSubmit,reset} = useForm<formType>()
   useEffect(()=>{
      (async ()=>{
         const {data} = await axios.get("http://localhost:3000/products")
-        // console.log(data);
         setProducts(data)
      })()
-  })
+  },[])
+  const onSubmit =async(formData:any)=>{
+      // console.log(data);
+      try {
+        const {data} = await axios.post("http://localhost:3000/products",formData) 
+        setProducts([...products,data])
+        reset()
+      } catch (error) {
+        console.log(error);
+        
+      }        
+  } 
   return (
-    <>   
+    <>
+    <form onSubmit={handleSubmit(onSubmit)}>
+        <input type='text' {...register("name")} placeholder='Tên sản phẩm'/>
+        <input type='text' {...register("image")} placeholder='Ảnh sản phẩm'/>
+        <input type='number' {...register("price")} placeholder='Giá sản phẩm'/>
+        <input type='text' {...register("category")} placeholder='Danh mục'/>
+        <button type='submit'>Thêm mới</button>  
+    </form>   
     <h1>Danh sách sản phẩm</h1> 
         <table>
             <thead>
