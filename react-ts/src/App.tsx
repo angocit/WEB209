154b03e7a4899440b9a9830fe0ea4edfb8c52ff1
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2024-07-08 19:03:18 +0700

    B4

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -15,6 +15,7 @@ type formType = Pick<IProduct,'name'|'price'|'image'|'category'>
 function App() {
   const [products,setProducts] = useState<IProduct[]>([])
   const {register,handleSubmit,reset} = useForm<formType>()
+  const [flag,setFlag] = useState<string|number>(0)
   useEffect(()=>{
      (async ()=>{
         const {data} = await axios.get("http://localhost:3000/products")
@@ -43,6 +44,36 @@ function App() {
       
     }
   }
+  const onSubmitUpdate =async(formData:any)=>{
+    // console.log(data);
+    try {
+      const {data} = await axios.put("http://localhost:3000/products/"+flag,formData)
+      //  console.log(data);
+       console.log(flag);
+       const newproduct = products.map((product:IProduct)=>{
+             if (product.id==flag){
+              product = data
+          }
+            return product
+       })      
+      setProducts(newproduct)
+      setFlag(0)
+      reset()
+    } catch (error) {
+      console.log(error);
+      
+    }        
+}
+  const onEdit = (id:number|string) => {
+    setFlag(id)
+    const product = products.filter((p:IProduct)=>p.id===id)
+    reset({
+        name:product[0].name,
+        image:product[0].image,
+        price:product[0].price,
+        category:product[0].category
+    })
+  }
   return (
     <>
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -65,12 +96,26 @@ function App() {
             </thead>
             <tbody>
                {products.map((product:IProduct,index)=>(
+                (product.id===flag)?
+                <tr key={product.id}>
+                  <td colSpan={5}>
+                  <form onSubmit={handleSubmit(onSubmitUpdate)}>
+                    <input type='text' {...register("name")} placeholder='Tên sản phẩm'/>
+                    <input type='text' {...register("image")} placeholder='Ảnh sản phẩm'/>
+                    <input type='number' {...register("price")} placeholder='Giá sản phẩm'/>
+                    <input type='text' {...register("category")} placeholder='Danh mục'/>
+                    <button type='submit'>Update</button>  
+                    <button type='button' onClick={()=>setFlag(0)}>Hủy</button>  
+                </form> 
+                </td>
+              </tr>
+                :
                 <tr key={product.id}>
                     <td>{index+1}</td>
                     <td>{product.name}</td>
                     <td>{product.category}</td>
                     <td>{product.price}</td>
-                    <td><button>Sửa</button><button onClick={()=>onDelete(product.id)}>Xóa</button></td>
+                    <td><button onClick={()=>onEdit(product.id)}>Sửa</button><button onClick={()=>onDelete(product.id)}>Xóa</button></td>
                 </tr>
                ))}
             </tbody>
