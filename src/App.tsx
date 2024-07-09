3c3638b7ec71bf9600bb96bec143d2b343ae1daf
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2024-07-09 17:06:53 +0700

    B4

--- src/App.tsx
+++ src/App.tsx
@@ -11,10 +11,12 @@ interface IProduct {
   price: number;
   category: string;
 }
+type FormData = Pick<IProduct,'name'|'image'|'price'|'category'>
 function App() {
   const [products,setProduct]=useState<IProduct[]>([])
-  const {register,handleSubmit,reset} = useForm()
+  const {register,handleSubmit,reset} = useForm<FormData>()
   const [isLoading,setLoading] = useState<boolean>(true)
+  const [flag,setFlag]=useState<number|string>(0)
   useEffect(()=>{
       (async ()=>{
         try {
@@ -38,6 +40,19 @@ function App() {
       }
       
   }
+  const onUpdate = async(product:any)=>{
+    // console.log(data);
+    try {
+        const {data} = await axios.put("http://localhost:3000/products/"+flag,product)
+        const newproduct = products.map(product=>(product.id===flag)?data:product)
+        setProduct(newproduct)
+        alert("Cập nhật thành công")
+        setFlag(0)
+    } catch (error) {
+        alert(error)
+    }
+    
+}
   const onDelete = async(id:number)=>{
      try {
       if (confirm("Are you sure")){
@@ -48,6 +63,16 @@ function App() {
      } catch (error) {
       
      }
+  }
+  const onEdit = (id:number|string)=>{
+    setFlag(id)
+    const [product] = products.filter(p=>p.id===id)
+    reset({
+      name:product.name,
+      image:product.image,
+      price:product.price,
+      category:product.category
+    })
   }
   return (
     <>
@@ -73,13 +98,23 @@ function App() {
       </thead>
       <tbody>
         {products.map((product:IProduct,index:number)=>(
+          (product.id===flag)?<tr>
+            <td colSpan={6}><form onSubmit={handleSubmit(onUpdate)}>
+            <input type='text' {...register("name")}/>
+            <input type='text' {...register("image")}/>
+            <input type='text' {...register("category")}/>
+            <input type='number' {...register("price")}/>
+            <button type='submit'>Cập nhật sản phẩm</button>
+            <button type='button' onClick={()=>setFlag(0)}>Hủy</button>
+            </form></td>           
+          </tr>:
           <tr key={product.id}>
               <td>{index+1}</td>
               <td><img width={90} src={product.image}/></td>
               <td>{product.name}</td>
               <td>{product.category}</td>
               <td>{product.price}</td>
-              <td><button>Sửa</button><button onClick={()=>onDelete(product.id)}>Xóa</button></td>
+              <td><button onClick={()=>onEdit(product.id)}>Sửa</button><button onClick={()=>onDelete(product.id)}>Xóa</button></td>
           </tr>
         ))}
       </tbody>
