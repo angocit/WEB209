1ebb04390ac3b206f4f6e8277ff49b4faa035092
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2024-07-12 20:19:58 +0700

    B6

--- react-ts/src/components/home.tsx
+++ react-ts/src/components/home.tsx
@@ -8,28 +8,31 @@ type Props = {
 const Home = ({products}: Props) => {
   return (
     <>
-      <table>
+    <div className='max-w-[1200px] mx-auto'>
+      <h1 className='text-[36px] text-center'>Danh sách sản phẩm</h1>
+      <table className='mx-auto w-[800px] table-auto border-collapse border border-slate-400'>
         <thead>
         <tr>
-          <th>STT</th>
-          <th>Ảnh</th>
-          <th>Tên sản phẩm</th>
-          <th>Giá tiền</th>
-          <th>Thao tác</th>
+          <th className='border border-slate-300'>STT</th>
+          <th className='border border-slate-300'>Ảnh</th>
+          <th className='border border-slate-300'>Tên sản phẩm</th>
+          <th className='border border-slate-300'>Giá tiền</th>
+          <th className='border border-slate-300'>Thao tác</th>
         </tr>
         </thead>
         <tbody>
           {products.map((product,index)=>(
             <tr key={product.id}>
-                <td>{index+1}</td>
-                <td><img width={90} src={product.image}/></td>
-                <td>{product.name}</td>
-                <td>{product.price}</td>
-                <td><button>Sửa</button><button>Xóa</button></td>
+                <td className='border border-slate-300'>{index+1}</td>
+                <td className='border border-slate-300'><img width={90} src={product.image}/></td>
+                <td className='border border-slate-300'>{product.name}</td>
+                <td className='border border-slate-300'>{product.price}</td>
+                <td className='border border-slate-300'><button>Sửa</button><button>Xóa</button></td>
             </tr>
           ))}
         </tbody>
       </table>
+      </div>
     </>
   )
 }
--- react-ts/src/layout/client/header.tsx
+++ react-ts/src/layout/client/header.tsx
@@ -1,14 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 type Props = {}
 
 const Header = (props: Props) => {
   return (
-    <div>
-        <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </div>
+    <header className='bg-slate-500'>
+        <div className='max-w-[1200px] mx-auto'>
+            <nav>
+              <ul className='flex gap-5'>
+                <li>
+                  <Link className='py-4 block text-white' to={'/'}>Trang chủ</Link>
+                </li>
+                <li>
+                  <Link className='py-4 block text-white' to={'/'}>Sản phẩm</Link>
+                </li>
+                <li>
+                  <Link className='py-4 block text-white' to={'/'}>Liên hệ</Link>
+                </li>
+              </ul>
+            </nav>
+        </div>
+    </header>
   )
 }
 
