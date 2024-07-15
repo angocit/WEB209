7a7dfad5b2134b5f6e5d9babbfa8bdcf4751d88d
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2024-07-15 19:19:36 +0700

    B7

--- react-ts/src/components/home.tsx
+++ react-ts/src/components/home.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { IProduct } from '../interface/product'
+import ProductItem from './productItem'
 
 type Props = {
   products:IProduct[]
@@ -10,29 +11,12 @@ const Home = ({products}: Props) => {
     <>
     <div className='max-w-[1200px] mx-auto'>
       <h1 className='text-[36px] text-center'>Danh sách sản phẩm</h1>
-      <table className='mx-auto w-[800px] table-auto border-collapse border border-slate-400'>
-        <thead>
-        <tr>
-          <th className='border border-slate-300'>STT</th>
-          <th className='border border-slate-300'>Ảnh</th>
-          <th className='border border-slate-300'>Tên sản phẩm</th>
-          <th className='border border-slate-300'>Giá tiền</th>
-          <th className='border border-slate-300'>Thao tác</th>
-        </tr>
-        </thead>
-        <tbody>
-          {products.map((product,index)=>(
-            <tr key={product.id}>
-                <td className='border border-slate-300'>{index+1}</td>
-                <td className='border border-slate-300'><img width={90} src={product.image}/></td>
-                <td className='border border-slate-300'>{product.name}</td>
-                <td className='border border-slate-300'>{product.price}</td>
-                <td className='border border-slate-300'><button>Sửa</button><button>Xóa</button></td>
-            </tr>
+      <div className='grid grid-cols-4 gap-x-6 gap-y-10'>
+          {products.map(product=>(
+            <ProductItem key={product.id} product={product}/>
           ))}
-        </tbody>
-      </table>
       </div>
+    </div>
     </>
   )
 }
