96e841ce7dcd1a9ea2f70fd4e5261331fc8c3d62
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2024-07-16 20:01:56 +0700

    b7

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
 
 type Props = {}
 
 const Header = (props: Props) => {
   return (
-    <div>Header</div>
+    <>
+      <header className='bg-slate-800 w-full'>
+        <nav className='max-w-[1200px] mx-auto'>
+          <ul className='flex gap-6'>
+            <li>
+              <NavLink className='text-white py-4 block' to={'/'}>Home</NavLink>
+            </li>
+            <li>
+              <NavLink className='text-white py-4 block' to={'/product-list'}>Sản phẩm</NavLink>
+            </li>
+            <li>
+              <NavLink className='text-white py-4 block' to={'/product/add'}>Thêm mới sản phẩm</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   )
 }
 
