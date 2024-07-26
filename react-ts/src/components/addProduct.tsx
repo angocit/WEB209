ba3429f95a98348966548977134def572d90c95c
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2024-07-26 19:51:10 +0700

    B12

--- react-ts/src/components/addProduct.tsx
+++ react-ts/src/components/addProduct.tsx
@@ -8,7 +8,7 @@ import { productCT } from '../context/productContext'
 
 const AddProduct = () => {
   const {onAdd} = useContext(productCT)
-    const {register,handleSubmit,reset} = useForm<formType>()
+    const {register,handleSubmit,reset,formState:{errors}} = useForm<formType>()
     const navigate = useNavigate()
     const onSubmit =async(formData:any)=>{
      await onAdd(formData)  
@@ -18,11 +18,26 @@ const AddProduct = () => {
   return (
     <>
     Thêm mới sản phẩm
-    <form onSubmit={handleSubmit(onSubmit)}>
-        <input type='text' {...register("name")} placeholder='Tên sản phẩm'/>
+    <form onSubmit={handleSubmit(onSubmit)} className='gap-4 flex flex-col'>
+        <input type='text' {...register("name",{required:true,minLength:6})} placeholder='Tên sản phẩm'/>
+        {(errors.name) &&
+          <p className='text-red-600 text-[12px]'>
+            Tên không được để trống và nhỏ hơn 6 ký tự
+          </p>
+        }
         <input type='text' {...register("image")} placeholder='Ảnh sản phẩm'/>
-        <input type='number' {...register("price")} placeholder='Giá sản phẩm'/>
-        <input type='text' {...register("category")} placeholder='Danh mục'/>
+        <input type='text' {...register("price",{required:true,pattern:/^\d*$/})} placeholder='Giá sản phẩm'/>
+        {(errors.price) &&
+          <p className='text-red-600 text-[12px]'>
+            Giá phải là số và không âm
+          </p>
+        }
+        <input type='text' {...register("category",{required:true,pattern:/^\S+@(\S+\.)+\S{2,6}$/})} placeholder='Danh mục'/>
+        {(errors.category) &&
+          <p className='text-red-600 text-[12px]'>
+            Email không đúng định dạng
+          </p>
+        }
         <button type='submit'>Thêm mới</button>  
     </form>  
     </>
