bb4a91a101eb52226e14b7d50cf17d288b906586
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2024-08-01 17:53:17 +0700

    usemutation

--- src/context/product.tsx
+++ src/context/product.tsx
@@ -2,7 +2,7 @@ import React, { createContext, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { IProduct,FormData } from '../interface/product'
 import { GetAllProducts, DeleteProductById, AddProduct, UpdateProduct } from '../services/product'
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 
 type Props = {
     children:React.ReactNode
@@ -17,6 +17,7 @@ const ProductContext = ({children}: Props) => {
   //        setProduct(data)     
   //     })()
   // },[])
+  const queryclient = useQueryClient()
   const {data:products,isLoading,isError} = useQuery({
     queryKey:['products'],
     queryFn: async ()=>{
@@ -24,39 +25,53 @@ const ProductContext = ({children}: Props) => {
       return products
     }
   })
+  const addmutation = useMutation({
+    mutationFn:async (data:FormData)=>{
+      await AddProduct(data) 
+    },
+    onSuccess:()=>{
+      alert('Thêm mới thành công')
+      navigate('/dashboard/product-list')
+      queryclient.invalidateQueries({queryKey:['products']})
+    }
+  })
+  const deletemutation = useMutation({
+    mutationFn:async (id:number|string)=>{
+      await DeleteProductById(id) 
+    },
+    onSuccess:()=>{
+      alert('Xóa thành công')
+      queryclient.invalidateQueries({queryKey:['products']})
+    }
+  })
   const deleteProduct = async (id:string|number)=>{
     if (confirm('Bạn chắc chứ?')){
-      try {
-          const data = await DeleteProductById(id)
-          alert('Xóa thành công')
-          // const newproducts = products.filter(product=>product.id !==id)
-          // setProduct(newproducts)
-      } catch (error) {
-        console.log(error);        
-      }
+      deletemutation.mutate(id)
     }
   }
-  const onAdd = async (data:FormData)=>{
-    try {
-        const product = await AddProduct(data)
-        alert('Thêm mới thành công')
-        // setProduct([...products,product])
-        navigate('/dashboard/product-list')
-    } catch (error) {
-      
+  const updatemutation = useMutation({
+    mutationFn:async (item:{data:FormData,id:number|string})=>{
+      await UpdateProduct(item.data,item.id)
+    },
+    onSuccess:()=>{
+      alert('Cập nhật thành công')
+      navigate('/dashboard/product-list')
+      queryclient.invalidateQueries({queryKey:['products']})
     }
+  })
+  const onAdd = async (data:FormData)=>{
+    addmutation.mutate(data)
+    // try {
+    //     const product = await AddProduct(data)
+    //     alert('Thêm mới thành công')
+    //     // setProduct([...products,product])
+    //     navigate('/dashboard/product-list')
+    // } catch (error) {
+      
+    // }
   }
   const onUpdate = async (data:FormData,id:number|string)=>{
-    try {
-        const dataproduct = await UpdateProduct(data,id)
-        alert('Cập nhật thành công')
-        // setProduct([...products,product])
-        // const newproducts = products.map(product=>(product.id==id)?dataproduct:product)
-        // setProduct(newproducts)
-        navigate('/dashboard/product-list')
-    } catch (error) {
-      
-    }
+    updatemutation.mutate({data:data,id:id})
   }
   return (
     <ProductCT.Provider value={{products,onUpdate,onAdd,deleteProduct,isLoading}}>
