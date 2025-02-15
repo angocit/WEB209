1cff86c23dd3c4fa6f3d4e1476e0efc7e88a0344
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2025-02-15 08:22:28 +0700

    b12

--- src/conponents/admin/sidebar.tsx
+++ src/conponents/admin/sidebar.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Sidebar = () => {
   return (
-    <div className='w-1/5 bg-white h-screen'>Sidebar</div>
+    <div className='w-1/5 bg-white h-screen p-4 [&_*]:text-[1.1rem] [&_*]:font-[600]'>
+        <ul>
+            <li>
+                <Link to={'/dashboard/add-product'}>Thêm mới danh mục</Link>
+            </li>
+            <li>
+                <Link to={'/dashboard/add-product'}>Thêm mới sản phẩm</Link>
+            </li>
+        </ul>
+    </div>
   )
 }
 
