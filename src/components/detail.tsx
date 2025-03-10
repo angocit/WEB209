b50028d899422bc2bb5ceb884f8d126e4426799f
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2025-03-10 18:22:55 +0700

    b1

--- src/components/detail.tsx
+++ src/components/detail.tsx
@@ -1,22 +1,47 @@
 import axios from 'axios'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useForm } from 'react-hook-form'
+import { useParams } from 'react-router-dom'
 
 type Props = {}
-
+type ICart = {
+    productId:number,
+    quantity:number
+}
 const DetailProduct = (props: Props) => {
-    const addToCart =(cartdata)=>{
+    const {register,handleSubmit,reset} = useForm<ICart>()
+    const params = useParams()
+    useEffect(()=>{
+        try {
+           ( async ()=>{
+            const {data} = await axios.get(`http://localhost:3000/products/${params.id}`);
+                reset({
+                    quantity:1,
+                    productId:data.id
+                })
+            })()
+        } catch (error) {
+            
+        }
+    },[])
+    const addToCart =async (cartdata:ICart)=>{
         //Lấy token
         const token = localStorage.getItem("token")
         const config = {
             headers: {"Authorization":"Bearer "+token}
         }
-        const {data} = axios.post("API giỏ hàng",cartdata,config)
+        const {data} = await axios.post("http://localhost:3000/carts",cartdata,config)
+        alert("Thêm giỏ hàng thành công")
+        console.log(data);
+        
     }
   return (
     <div>
         <h1>THông tin mô tả sản phẩm</h1>
-        <form>
-            có 2 input chứa productId và quantity
+        <form onSubmit={handleSubmit(addToCart)}>
+           <input type='text' {...register("productId")}/>
+           <input type='text' {...register("quantity")}/>
+           <button>Thêm giỏ hàng</button>
         </form>
         </div>
   )
