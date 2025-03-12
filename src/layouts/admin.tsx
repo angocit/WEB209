c310820f9d96f1f0ee653367c26f07b553ba1255
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2025-03-12 17:37:00 +0700

    b2

--- src/layouts/admin.tsx
+++ src/layouts/admin.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import AdminHeader from '../components/admin/header'
+import AdminSidebar from '../components/admin/sidebar'
+import { Outlet } from 'react-router-dom'
 
 const AdminLayout = () => {
   return (
-    <div>AdminLayout</div>
+    <main className='bg-[#f6f9ff]'>
+        <AdminHeader/>
+        <div className='flex'>
+        <AdminSidebar/>
+        <div className='content w-4/5'>
+            <Outlet/>
+        </div>
+        </div>
+    </main>
   )
 }
 
