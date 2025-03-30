aab000ee2c992a7d353cb05cb4998e670f6d8a4e
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2025-03-30 15:21:12 +0700

    b9: sử dụng useReducer

--- src/components/client/home.tsx
+++ src/components/client/home.tsx
@@ -1,10 +1,21 @@
 import { useQuery } from '@tanstack/react-query'
-import React from 'react'
+import React, { useReducer } from 'react'
 import { IProduct } from '../../interface/product'
 import { api } from '../../config/axios'
 import ProductItem from './products/item'
 
 const Home = () => {
+  const reducer = (state:number,action:{type:string})=>{
+      switch (action.type){
+          case "tang":
+            return state+1
+          case "giam":
+           return state-1
+          default:
+           return state
+      }
+  }
+  const [count,dispatch] = useReducer(reducer,0)
   const {data,isLoading} = useQuery<IProduct[]>({
     queryKey: ["products"],
     queryFn: async ()=>{
@@ -21,6 +32,10 @@ const Home = () => {
   }
   return (
     <>
+      <p>Giá trị hiện tại: {count}</p>
+
+      <button onClick={()=>dispatch({type:"tang"})}>Tăng</button>
+      <button  onClick={()=>dispatch({type:"giam"})}>Giảm</button>
       <h1 className='text-[1.2rem] text-center my-4'>Sản phẩm bán chạy</h1>
       <div className='grid grid-cols-4 gap-4'>
       {
