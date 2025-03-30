a851b33453d2c028261c98aa22ebf4abc5bf8d82
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2025-03-30 12:40:34 +0700

    b9: Sử dụng useReducer

--- src/components/client/home.tsx
+++ src/components/client/home.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query'
-import React, { useState } from 'react'
+import React, { useReducer, useState } from 'react'
 import { IProduct } from '../../interface/product'
 import { ListData } from '../../services/data'
 import ItemProduct from './product/item'
@@ -7,6 +7,17 @@ import Counter from './counter/counter'
 
 const HomeClient = () => {
     const [counter,setCounter] = useState<number>(0)
+    const reducer = (state:number,action:{type:string,payload:{value:number}})=>{
+        switch (action.type){
+            case "tang":
+                return state+action.payload.value
+            case "giam":
+                return state-action.payload.value
+            default:
+                return state
+        }
+    }
+    const [count, dispatch] = useReducer(reducer,0)   
     const {data,isLoading} = useQuery<IProduct[]>({
         queryKey:["products"],
         queryFn: async ()=>{
@@ -20,7 +31,11 @@ const HomeClient = () => {
   return (
     <div>
         <h2>Khu vực hiển thị counter</h2>
-        <Counter counter={counter} setCounter={setCounter}/>
+        <span>Số hiện tại là: {count}</span>
+
+        <button onClick={()=>dispatch({type:"tang",payload:{value:2}})}>Tăng giá trị</button>
+        <button onClick={()=>dispatch({type:"giam",payload:{value:1}})}>Giảm giá trị</button>
+        {/* <Counter counter={counter} setCounter={setCounter}/> */}
         <h2>Sản phẩm nổi bật</h2>
         <div className='grid grid-cols-5'>
         {
