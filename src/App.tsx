bfa8b42a4a86ea9f41b759a3f6d39e95af182371
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2025-11-08 19:06:22 +0700

    b3:Sử dụng useRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import { Route, Routes, useRoutes } from "react-router-dom"
 import Home from "./pages/Home"
 import Category from "./pages/Category"
+import ClientLayout from "./layouts/client/ClientLayout"
+import AdminLayout from "./layouts/admin/AdminLayout"
+import Dashboard from "./pages/admin/Dashboard"
 
 function App() {
   const router = useRoutes([
-    {path:'/',Component:Home},
-    {path:'/category',Component:Category}
+    {path:'/',Component:ClientLayout,children:[
+      {path:'',Component:Home},
+      {path:'category',Component:Category}
+    ]},
+    {path:'/admin',Component:AdminLayout,children:[
+      {path:'',Component:Dashboard}
+    ]}
+    // {path:'/category',Component:Category}
   ]) 
   return router
 }
