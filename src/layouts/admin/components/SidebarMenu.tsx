6fcab19fc8bc4caad25f4ecc8dc3bb080b718fb0
unknown <angocit@gmail.com>
2025-11-11 20:05:35 +0700

    b4:Sử dụng layout trong antdesign

--- src/layouts/admin/components/SidebarMenu.tsx
+++ src/layouts/admin/components/SidebarMenu.tsx
@@ -1,59 +1,48 @@
-import React from 'react'
+import React from "react";
 import {
   AppstoreOutlined,
   ContainerOutlined,
+  DashboardOutlined,
   DesktopOutlined,
   MailOutlined,
   MenuFoldOutlined,
   MenuUnfoldOutlined,
   PieChartOutlined,
-} from '@ant-design/icons';
-import type { MenuProps } from 'antd';
-import { Button, Menu } from 'antd';
-type MenuItem = Required<MenuProps>['items'][number];
+  ProductOutlined,
+} from "@ant-design/icons";
+import type { MenuProps } from "antd";
+import { Button, Menu } from "antd";
+import { useNavigate } from "react-router-dom";
+type MenuItem = Required<MenuProps>["items"][number];
 const SidebarMenu = () => {
-    const items: MenuItem[] = [
-  { key: '1', icon: <PieChartOutlined />, label: 'Option 1' },
-  { key: '2', icon: <DesktopOutlined />, label: 'Option 2' },
-  { key: '3', icon: <ContainerOutlined />, label: 'Option 3' },
-  {
-    key: 'sub1',
-    label: 'Navigation One',
-    icon: <MailOutlined />,
-    children: [
-      { key: '5', label: 'Option 5' },
-      { key: '6', label: 'Option 6' },
-      { key: '7', label: 'Option 7' },
-      { key: '8', label: 'Option 8' },
-    ],
-  },
-  {
-    key: 'sub2',
-    label: 'Navigation Two',
-    icon: <AppstoreOutlined />,
-    children: [
-      { key: '9', label: 'Option 9' },
-      { key: '10', label: 'Option 10' },
-      {
-        key: 'sub3',
-        label: 'Submenu',
-        children: [
-          { key: '11', label: 'Option 11' },
-          { key: '12', label: 'Option 12' },
-        ],
-      },
-    ],
-  },
-];
+  const items: MenuItem[] = [
+    {key:'',label:'Dashboard',icon:<DashboardOutlined />},
+    {
+      key: "product",
+      icon: <ProductOutlined />,
+      label: "QL sản phẩm",
+      children: [
+        { key: "products/add", label: "Thêm mới sản phẩm" },
+        { key: "products", label: "Danh sách sản phẩm" },
+        { key: "products/properties", label: "Quản lý thuộc tính" },
+      ],
+    },
+  ];
+  const navigate = useNavigate();
+  const handleClick = ({ key }: any) => {
+    console.log(key);
+    navigate(`/admin/${key}`);
+  };
   return (
     <Menu
-        defaultSelectedKeys={['1']}
-        defaultOpenKeys={['sub1']}
-        mode="inline"
-        theme="dark"
-        items={items}
-      />
-  )
-}
+      defaultSelectedKeys={["1"]}
+      defaultOpenKeys={["sub1"]}
+      mode="inline"
+      theme="dark"
+      items={items}
+      onClick={handleClick}
+    />
+  );
+};
 
-export default SidebarMenu
+export default SidebarMenu;
