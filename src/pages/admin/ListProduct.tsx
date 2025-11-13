2c653874b2a6b21fd548b7ff317bf902d6b3c90c
unknown <angocit@gmail.com>
2025-11-13 19:39:48 +0700

    b5:ĐK Call API useQuery

--- src/pages/admin/ListProduct.tsx
+++ src/pages/admin/ListProduct.tsx
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
 import axios from 'axios'
 import React from 'react'
 import type { IProduct } from '../../types/product'
@@ -6,18 +6,23 @@ import type { IProduct } from '../../types/product'
 type Props = {}
 
 const ListProduct = (props: Props) => {
+    const queryclient = useQueryClient()
   const {data,isLoading,isError} = useQuery<IProduct[]>({
     queryKey: ["AllProduct"],
     queryFn: async ()=>{
       const {data} = await axios.get("http://localhost:3000/products")
       return data
-    }
+    },
+    // enabled: !queryclient.getQueryData(["AllProduct"])
+     staleTime:Infinity
   })
   if (isLoading) return <>Loading</>
   return (
     <div>
        {data&&data.map(item=>(
+        <>
         <div key={item.id}>{item.name}</div>
+        </>
        ))}
     </div>
   )
