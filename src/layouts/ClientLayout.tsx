241d4004b220f5caf4950684c33ac7d97e37f83f
angocit <angocit@gmail.com>
2026-03-14 17:29:17 +0700

    b3: chia router và layout

---
+++ src/layouts/ClientLayout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+const ClientLayout = () => {
+  return (
+    <div>
+        <h1>Client Layout</h1>
+        <Outlet/>
+    </div>
+  )
+}
+
+export default ClientLayout
---
+++ src/pages/admin/Products.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Products = () => {
+  return (
+    <div>Danh sách sản phẩm</div>
+  )
+}
+
+export default Products
