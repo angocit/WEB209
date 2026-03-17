31b00ec888edf6093bf8a608a46e949500f6fe41
angocit <angocit@gmail.com>
2026-03-17 17:55:54 +0700

    b4: sử dụng useQuery

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,34 @@
+import { useQuery, useQueryClient } from '@tanstack/react-query'
+import axios from 'axios'
 import React from 'react'
+import { IProduct } from '../interface/product'
 
 const Home = () => {
+  const queryclient = useQueryClient()
+  const {data,isLoading} = useQuery<IProduct[]>({
+    queryKey:['products'],
+    queryFn: async ()=>{
+        const {data} = await axios.get(`http://localhost:3000/product`)
+        return data
+    },
+    // staleTime:Infinity // Chỉ chạy 1 lần duy nhất
+    enabled:!queryclient.getQueryData(['products']) // Chỉ chạy khi chưa có dữ liệu
+  })
+  if (isLoading) return <>Đang tải dữ liệu</>
   return (
-    <div>Home</div>
+    <>
+    <div className='max-w-7xl mx-auto'>
+      <h2>Danh sách sản phẩm</h2>
+      <div className='grid grid-cols-4'>
+          {data&&data.map((product)=>(
+            <div>
+              <img src={product.image}/>
+              <h3>{product.name}</h3>
+            </div>
+          ))}
+      </div>
+      </div>
+    </>
   )
 }
 
